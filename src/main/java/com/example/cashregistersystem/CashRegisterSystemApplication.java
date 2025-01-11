97ad13e1ce754085759192496c3105c298307f1f
Felix Erhard <felix.erhard@sap.com>
2025-01-11 11:15:00 +0100

    initial commit

---
+++ src/main/java/com/example/cashregistersystem/CashRegisterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashregistersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRegisterSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashRegisterSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cashregistersystem/CashRegisterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashregistersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashRegisterSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
