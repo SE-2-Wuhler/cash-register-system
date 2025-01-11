9e374a398d20c71f4913b7856e510b1b5bdb6af9
Felix Erhard <felix.erhard@sap.com>
2025-01-11 11:24:20 +0100

    Changed baseURL to com.se

--- src/main/java/de/se/cashregistersystem/CashRegisterSystemApplication.java
+++ src/main/java/de/se/cashregistersystem/CashRegisterSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.cashregistersystem;
+package de.se.cashregistersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/de/se/cashregistersystem/CashRegisterSystemApplicationTests.java
+++ src/test/java/de/se/cashregistersystem/CashRegisterSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cashregistersystem;
+package de.se.cashregistersystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
